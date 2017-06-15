84c458cb1b5a27dc501471c7374113b1768e62bb
Matthew Staples <staplma2@uw.edu>
2017-06-15 11:47:48 -0700

    Removes clickability from 'Matthew Staples' and makes mobile menu hide when making a selection

--- articleView.js
+++ articleView.js
@@ -1,14 +1,20 @@
 'use strict';
 
 function handleTabs() {
-  $('.top-header .tab:first, .top-header li:first, .nav .tab:first').click(function() {
+  $('.top-header .tab:first, .nav .tab:first').click(function() {
     $('#proj').show();
     $('#about').hide();
+    if($(window).width() < 640) {
+      $('.nav').toggleClass('clicked');
+    }
   });
 
   $('.top-header .tab:last, .nav .tab:last').click(function() {
     $('#proj').hide();
     $('#about').show();
+    if($(window).width() < 640) {
+      $('.nav').toggleClass('clicked');
+    }
   });
 }
 
