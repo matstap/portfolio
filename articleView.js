21e3098cc450d5e66075526c53984113e9526cad
Matthew Staples <staplma2@uw.edu>
2017-06-15 11:37:55 -0700

    Changes #articles to more appropriately named #proj and adds projects tab to clickable selection

--- articleView.js
+++ articleView.js
@@ -1,15 +1,13 @@
 'use strict';
 
-
-
 function handleTabs() {
-  $('.top-header .tab:first').click(function() {
-    $('#articles').show();
+  $('.top-header .tab:first, .top-header li:first, .nav .tab:first').click(function() {
+    $('#proj').show();
     $('#about').hide();
   });
 
-  $('.top-header .tab:last').click(function() {
-    $('#articles').hide();
+  $('.top-header .tab:last, .nav .tab:last').click(function() {
+    $('#proj').hide();
     $('#about').show();
   });
 }
