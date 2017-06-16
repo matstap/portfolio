3a92f5291ef9f1b028b4a34a663d7b560855d90e
Matthew Staples <staplma2@uw.edu>
2017-06-16 09:45:12 -0700

    Adds handlebars script and template for projects

--- article.js
+++ article.js
@@ -11,19 +11,21 @@ function Project(rawData){
 }
 
 Project.prototype.toHtml = function() {
-  var $newProject = $('article.template').clone();
-  $newProject.removeClass('template');
+  
 
-  $newProject.find('h1 a').html(this.title);
-  $newProject.find('h1 a').attr('href', this.url);
-  console.log(this.url);
-  $newProject.find('.byline span').html(this.collabs);
-  $newProject.find('.article-body').html(this.body);
-  $newProject.find('time').attr('pubdate', this.lastDate);
-
-  $newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.lastDate))/60/60/24/1000) + ' days ago');
-  $newProject.append('<hr>');
-  return $newProject;
+  // var $newProject = $('article.template').clone();
+  // $newProject.removeClass('template');
+  //
+  // $newProject.find('h1 a').html(this.title);
+  // $newProject.find('h1 a').attr('href', this.url);
+  // console.log(this.url);
+  // $newProject.find('.byline span').html(this.collabs);
+  // $newProject.find('.article-body').html(this.body);
+  // $newProject.find('time').attr('pubdate', this.lastDate);
+  //
+  // $newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.lastDate))/60/60/24/1000) + ' days ago');
+  // $newProject.append('<hr>');
+  // return $newProject;
 };
 
 rawData.sort(function(a,b) {
