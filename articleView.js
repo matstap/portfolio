aecab18c0b51b9d0d647e4c77fc93b0654878751
Matthew Staples <staplma2@uw.edu>
2017-06-15 21:51:42 -0700

    adds home clss to 'Matthew Staples' and logic in handleTabs to fade in all sections when clicked

--- articleView.js
+++ articleView.js
@@ -3,13 +3,19 @@
 function handleTabs() {
   $('.top-header, .nav').on('click', '.tab', function() {
     $('.tab-content').hide();
+
     $('#' + $(this).data('content')).fadeIn();
     if($(window).width() < 640) {
       $('.nav').toggleClass('clicked');
     }
   });
 
-  $('.main-nav .tab:first').click();
+  $('top-header .tab, .nav .tab').click();
+
+  $('.home').click(function() {
+    $('main section').hide();
+    $('main section').fadeIn();
+  });
 }
 
 $(document).ready(function() {
