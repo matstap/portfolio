5ecfca778ae38262fbf4f514d21122be9cec630d
Matthew Staples <staplma2@uw.edu>
2017-06-27 09:42:27 -0700

    removes handleTabs and references, script tags to controllers, and assigns href values

--- public/scripts/articleView.js
+++ public/scripts/articleView.js
@@ -4,24 +4,24 @@ var app = app || {};
 (function(module) {
   var articleView = {};
 
-  articleView.handleTabs = function() {
-    $('.tab').click(function() {
-      $('.tab-content').hide();
-      $('#' + $(this).data('content')).fadeIn();
-      if($(window).width() < 640) {
-        $('.nav').toggleClass('clicked');
-      }
-    });
-
-    $('.home').click(function() {
-      $('main section').hide();
-      $('main section').fadeIn();
-    });
-  };
+  // articleView.handleTabs = function() {
+  //   $('.tab').click(function() {
+  //     $('.tab-content').hide();
+  //     $('#' + $(this).data('content')).fadeIn();
+  //     if($(window).width() < 640) {
+  //       $('.nav').toggleClass('clicked');
+  //     }
+  //   });
+  //
+  //   $('.home').click(function() {
+  //     $('main section').hide();
+  //     $('main section').fadeIn();
+  //   });
+  // };
 
   articleView.initIndex = function() {
     app.Project.all.forEach(project => $('#proj').append(project.toHtml()));
-    articleView.handleTabs();
+    // articleView.handleTabs();
     $('#proj .number').text(`${app.Project.numCollabs()} collaborators`);
   };
 
